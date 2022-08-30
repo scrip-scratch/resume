cb7101e39000a5c49469fd2e666a5bf6b6711a74
scripscratch <scripscratch@mail.ru>
2022-08-30 19:48:37 +0800

    add drag

--- scripts/slider.js
+++ scripts/slider.js
@@ -7,12 +7,15 @@ function sliderAction (slider, sliderItem, dots) {
   let sliderOffset = slider.offsetLeft;
   let sliderLength = slider.children.length;
   let slideWidth = sliderItem.offsetWidth;
+  let posInitial, posFinal, posX1 = 0, posX2 = 0;
+  let threshold = slideWidth / 15;
   let index = 0;
   
   addDots();
 
-  
-  function checkIndex (index) {
+  slider.addEventListener('transitionend', checkIndex);
+
+  function checkIndex () {
     activeDot(index);
   } 
   
@@ -31,7 +34,6 @@ function sliderAction (slider, sliderItem, dots) {
     index = Array.from(dots.children).indexOf(dot);
     sliderOffset = -slideWidth * index;
     slider.style.left = sliderOffset + 'px';
-    checkIndex(index);
   }
   
   function activeDot (index) {
@@ -41,16 +43,65 @@ function sliderAction (slider, sliderItem, dots) {
     dots.children[index].classList.add('cards_dot_active');
   }
 
+  function move (dest) {
+    if (dest === -1) {
+      sliderOffset += slideWidth;
+      slider.style.left = sliderOffset + 'px';
+    }
+    if (dest === 1) {
+      sliderOffset -= slideWidth;
+      slider.style.left = sliderOffset + 'px';
+    }
+    index = Math.round(Math.abs(sliderOffset / slideWidth));
+  }
+
   window.addEventListener('resize', () => {
     setTimeout(() => {
       slideWidth = sliderItem.offsetWidth;
       index = 0;
       sliderOffset = -slideWidth * index;
       slider.style.left = sliderOffset + 'px';
-      checkIndex(index);
     }, 300)
   })
+
+  slider.onmousedown = dragStart;
+  
+  slider.addEventListener('touchstart', dragStart);
+  slider.addEventListener('touchmove', dragAction);
+  slider.addEventListener('touchend', dragEnd);
+   
+  function dragStart (e) {
+    e.preventDefault();
+    posInitial = slider.offsetLeft;
+    posX1 = e.changedTouches[0].clientX;
+    document.onmouseup = dragEnd;
+    document.onmousemove = dragAction;
+  }
+
+  function dragAction (e) {
+    posX2 = posX1 - e.changedTouches[0].clientX;
+    posX1 = e.changedTouches[0].clientX;
+    console.log(posX2);
+    slider.style.left = (slider.offsetLeft - posX2 * 8) + "px";
+  }
+  
+  function dragEnd () {
+    posFinal = slider.offsetLeft;
+    console.log(posFinal);
+    if ((posFinal - posInitial < -threshold) && (index < sliderLength - 1)) {
+      move(1);
+    } else if ((posFinal - posInitial > threshold) && (index > 0)) {
+      move(-1);
+    } else {
+      slider.style.left = (posInitial) + "px";
+    }
+
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
   
 }
+
+
 
 sliderAction(slider, sliderItem, dots);
